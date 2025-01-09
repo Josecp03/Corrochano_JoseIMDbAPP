c3ac17c298d94367c4cca18b328f01fbb1dc015c
José Corrochano Pardo <118207818+Josecp03@users.noreply.github.com>
2025-01-09 18:12:11 +0100

    3ªFuncionalidad (Enviar SMS)

--- app/src/main/java/edu/pmdm/corrochano_josimdbapp/MovieDetailsActivity.java
+++ app/src/main/java/edu/pmdm/corrochano_josimdbapp/MovieDetailsActivity.java
@@ -1,13 +1,23 @@
 package edu.pmdm.corrochano_josimdbapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -27,13 +37,20 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 public class MovieDetailsActivity extends AppCompatActivity {
 
     private Movie pelicula;
-    private TextView txt;
+    private TextView txtTitle = null;
+    private TextView txtDescription = null;
+    private TextView txtDate = null;
     private IMDBApiService imdbApiService;
-    private TextView txt2;
     private ImageView imagen;
+    private Button btnSMS = null;
+    private double rating;
+
+    private static final int PERMISSION_REQUEST_CODE_CONTACTS = 101;
+    private static final int PERMISSION_REQUEST_CODE_SMS = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +66,15 @@ public class MovieDetailsActivity extends AppCompatActivity {
         Intent i = getIntent();
         pelicula = i.getParcelableExtra("pelicula");
 
-        txt = findViewById(R.id.TextViewTitle);
-        txt2 = findViewById(R.id.TextViewDescription);
-        TextView releaseDateView = findViewById(R.id.TextViewDate); // Referencia al TextView de la fecha
-        txt.setText(pelicula.getTitle());
+        txtTitle = findViewById(R.id.TextViewTitle);
+        txtDescription = findViewById(R.id.TextViewDescription);
+        txtDate = findViewById(R.id.TextViewDate);
+        btnSMS = findViewById(R.id.btnSendSms);
         imagen = findViewById(R.id.ImageViewPortada);
 
-        // Cargar la imagen del poster usando Glide
+        txtTitle.setText(pelicula.getTitle());
+
+        // Cargar la imagen de la portada usando Glide
         Glide.with(this)
                 .load(pelicula.getPosterPath())
                 .into(imagen);
@@ -88,19 +107,19 @@ public class MovieDetailsActivity extends AppCompatActivity {
             public void onResponse(Call<MovieOverviewResponse> call, Response<MovieOverviewResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     String descripcion = response.body().getData().getTitle().getPlot().getPlotText().getPlainText();
-                    txt2.setText(descripcion);
+                    txtDescription.setText(descripcion);
 
                     // Obtener y formatear la fecha de lanzamiento
                     MovieOverviewResponse.ReleaseDate releaseDate = response.body().getData().getTitle().getReleaseDate();
                     if (releaseDate != null) {
                         String formattedDate = String.format("%d-%02d-%02d", releaseDate.getYear(), releaseDate.getMonth(), releaseDate.getDay());
-                        releaseDateView.setText("Release Date: " + formattedDate);
+                        txtDate.setText("Release Date: " + formattedDate);
                     }
 
                     // Obtener y mostrar el rating
                     MovieOverviewResponse.RatingsSummary ratingsSummary = response.body().getData().getTitle().getRatingsSummary();
                     if (ratingsSummary != null) {
-                        double rating = ratingsSummary.getAggregateRating();
+                        rating = ratingsSummary.getAggregateRating();
                         TextView ratingView = findViewById(R.id.TextViewRating);
                         ratingView.setText("Rating: " + String.format("%.1f", rating));
                     }
@@ -114,5 +133,121 @@ public class MovieDetailsActivity extends AppCompatActivity {
             }
         });
 
+        btnSMS.setOnClickListener(v -> {
+
+            // Verificar permisos de contactos
+            if (ContextCompat.checkSelfPermission(MovieDetailsActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MovieDetailsActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, PERMISSION_REQUEST_CODE_CONTACTS);
+            } else if (ContextCompat.checkSelfPermission(MovieDetailsActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                // Verificar permisos de SMS
+                ActivityCompat.requestPermissions(MovieDetailsActivity.this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE_SMS);
+            } else {
+                // Lanzar el método cuando ambos permisos están concedidos
+                sendSms();
+            }
+
+        });
+
+    }
+
+    private void sendSms() {
+        // Crear y lanzar el intent
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PERMISSION_REQUEST_CODE_CONTACTS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE_CONTACTS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE_SMS);
+            } else {
+                Toast.makeText(this, "Permiso de contactos denegado.", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == PERMISSION_REQUEST_CODE_SMS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendSms();
+            } else {
+                Toast.makeText(this, "Permiso para enviar SMS denegado.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PERMISSION_REQUEST_CODE_CONTACTS && resultCode == RESULT_OK && data != null) {
+            Uri contactUri = data.getData();
+            String idContacto = obtenerIdContacto(contactUri);
+            if (idContacto != null) {
+                String numTelefono = obtenerTelefono(idContacto);
+                if (numTelefono != null && !numTelefono.isEmpty()) {
+
+                    // Construir el mensaje para el SMS con el rating formateado
+                    String textoSMS = "Esta película te gustará: " + txtTitle.getText().toString() +
+                            " Rating: " + String.format("%.1f", rating);
+
+                    // Enviar el SMS con los detalles de la película
+                    enviarSMS(numTelefono, textoSMS);
+                } else {
+                    Toast.makeText(this, "El contacto no tiene número de teléfono.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void enviarSMS(String numero, String texto) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE_SMS);
+        } else {
+            abrirAppSMS(numero, texto);
+        }
+    }
+
+    private void abrirAppSMS(String numero, String texto) {
+        if (numero == null || texto == null || numero.isEmpty() || texto.isEmpty()) {
+            Toast.makeText(this, "No se tiene número o texto para enviar.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+        smsIntent.setData(Uri.parse("smsto:" + numero));
+        smsIntent.putExtra("sms_body", texto);
+        startActivity(smsIntent);
+    }
+
+    private String obtenerIdContacto(Uri contactUri) {
+        String idContacto = null;
+        Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+            idContacto = cursor.getString(idIndex);
+        }
+        if (cursor != null) {
+            cursor.close();
+        }
+        return idContacto;
+    }
+
+    private String obtenerTelefono(String contactId) {
+        String numTelefono = null;
+        Cursor cursorTelefono = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                new String[]{contactId},
+                null
+        );
+        if (cursorTelefono != null && cursorTelefono.moveToFirst()) {
+            int numberIndex = cursorTelefono.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            numTelefono = cursorTelefono.getString(numberIndex);
+        }
+        if (cursorTelefono != null) {
+            cursorTelefono.close();
+        }
+        return numTelefono;
     }
 }
